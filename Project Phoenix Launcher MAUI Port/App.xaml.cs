67cc872ad7715bdab2aa390662aaf66f813cba22
Type-32 <wilsonsu2008@hotmail.com>
2023-02-05 18:53:12 +0800

    Init

---
+++ Project Phoenix Launcher MAUI Port/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Project_Phoenix_Launcher_MAUI_Port;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Project Phoenix Launcher MAUI Port/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Project_Phoenix_Launcher_MAUI_Port;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
